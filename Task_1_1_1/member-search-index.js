9b2356f3e676e321c95f622edc68e22c1f2dc2e2
Artemius951 <Artemius951@users.noreply.github.com>
2025-09-03 15:44:28 +0000

    Deploying to gh-pages from @ Artemius951/OOP@897591b25b803f15b6a09c8569c6257152c84017 🚀

--- Task_1_1_1/member-search-index.js
+++ Task_1_1_1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"ru.nsu.kutsenko.Task_1_1_1","c":"Sort","l":"Sort()","u":"%3Cinit%3E()"},{"p":"ru.nsu.kutsenko.Task_1_1_1","c":"Sort","l":"sort(int[])"}];updateSearchResults();
+memberSearchIndex = [{"p":"ru.nsu.kutsenko.Task_1_1_1","c":"Sort","l":"check(int[])"},{"p":"ru.nsu.kutsenko.Task_1_1_1","c":"Sort","l":"heapSort(int[])"},{"p":"ru.nsu.kutsenko.Task_1_1_1","c":"Sort","l":"Sort()","u":"%3Cinit%3E()"}];updateSearchResults();
