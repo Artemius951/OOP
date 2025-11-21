44f917ca6cb8672864df4bfe1a987f5715ec4eda
Artemius951 <a.kutsenko1@g.nsu.ru>
2025-11-21 22:25:57 +0700

    edit

--- Task_1_2_1/src/main/java/ru/nsu/kutsenko/task121/GparsException.java
+++ Task_1_2_1/src/main/java/ru/nsu/kutsenko/task121/GparsException.java
@@ -3,12 +3,12 @@ package ru.nsu.kutsenko.task121;
 /**
  * Исключение для ошибок формата файла графа.
  */
-public class GParsException extends RuntimeException {
-    public GParsException(String mes) {
+public class GparsException extends RuntimeException {
+    public GparsException(String mes) {
         super(mes);
     }
 
-    public GParsException(String mes, Throwable cause) {
+    public GparsException(String mes, Throwable cause) {
         super(mes, cause);
     }
 }
