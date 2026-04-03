0e1b5d2294898f12c8680c0c191bf0db1be0a0bb
Artemius951 <a.kutsenko1@g.nsu.ru>
2026-04-03 21:47:02 +0700

    warnings

--- Task_2_3_1/src/main/java/ru/nsu/kutsenko/task231/Game.java
+++ Task_2_3_1/src/main/java/ru/nsu/kutsenko/task231/Game.java
@@ -1,7 +1,7 @@
 package ru.nsu.kutsenko.task231;
 
-import javafx.application.Application;
 import java.io.IOException;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
